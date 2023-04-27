145cb884340422b580b67d76445d4ea0ea82fb51
Shubham Goyal <goyalshubham247@gmail.com>
2023-04-27 15:54:03 +0530

    offer and signup validation

--- src/components/mobile/Signup.js
+++ src/components/mobile/Signup.js
@@ -1,13 +1,36 @@
-import React from "react";
+
+import React, { useRef, useState } from "react";
 import "../../assets/styles/mobile/Signup.css";
 import "../../assets/styles/mobile/global.css";
 import { FcGoogle } from "react-icons/fc";
 // import { FcGoogle } from "react-icons/fc"; 
+import Validation from "./Validation";
 
 
-function Signup() {
+function Signup() 
+  {
+    const [isfirstRender, setIsfirstRender] = useState(true);
+  
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [count, setCount] = useState(0);
+  
+    function validation() {
+      alert("clicked")
+      setCount(count + 1);
+    }
   return (
     <div className="wrapper">
+      {count}
+      {count != 0 ? (
+        <Validation
+          email={emailRef.current.value}
+          password={passwordRef.current.value}
+          count={count}
+        />
+      ) : (
+        <div></div>
+      )}
       <div className="top-div">
         <img className="barber-img" src="https://imgs.search.brave.com/Bd-v6phDCm1VzMf18bKW6HFI9x0hBowgXT0ufjHo230/rs:fit:485:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC54/SGRqZldjN2xSR25r/M096bGRaVlBBSGFI/UCZwaWQ9QXBp" alt="Barber" />
         <div className="signup-acc">Create Account </div>
@@ -20,6 +43,7 @@ function Signup() {
           <input
             className="input-text"
             type="email"
+            ref={emailRef}
             placeholder="Enter your email or phone number"
           />
         </div>
@@ -36,12 +60,13 @@ function Signup() {
           <input 
             className="input-text"
             type="password"
+            ref={passwordRef}
             placeholder="Confirm your Password"
           />
         </div>
 
         <div className="signup-btn">
-          <button className="btn-design" >Signup</button>
+          <button className="btn-design" onClick={validation}>Signup</button>
         </div>
         <div className="signup-container-login-link">Already hava an account? <a href="https://example.com/faq.html"> Login </a></div>
       
