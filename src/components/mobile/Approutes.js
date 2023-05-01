818029a1cdd439ebe786a9dcef8776daf06ea4ff
Aashutosh Soni <soniaashu083@gmail.com>
2023-05-01 20:58:37 +0530

    Added States and Change handler to Business, Offersjs concverted to Facilitiesjs

--- src/components/mobile/Approutes.js
+++ src/components/mobile/Approutes.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-// import Loginform from "./Loginform";
+import Loginform from "./Loginform";
 import Header from "./Header";
 import Showsalon from "./Showsalon";
 import Changingtext from "./Changingtext"
@@ -9,7 +9,8 @@ import Search from "./Search";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "./demo.css";
-import Offers from "./Offers";
+import Facilities from "./Facilities";
+import Business from "./Business";
 
 function Approutes() {
   return (
@@ -17,11 +18,13 @@ function Approutes() {
     <Router>
       <Routes>
         <Route exact path="/" element={<div>hello</div>}></Route>
-        <Route exact path="/login" element={<Loginform />}></Route>
-        <Route exact path="/signup" element={<Signup />}></Route>
-        <Route exact path="/services" element={<Showsalon />}></Route>
-        <Route exact path="/search" element={<Search />}></Route>
-        <Route exact path="/card" element={<Card />}></Route>
+        <Route exact path="/login" element={<Loginform/>}></Route>
+        <Route exact path="/signup" element={<Signup/>}></Route>
+        <Route exact path="/services" element={<Showsalon/>}></Route>
+        <Route exact path="/search" element={<Search/>}></Route>
+        {/* <Route exact path="/card" element={<Card/>}></Route>  */}
+        <Route exact path="/offers" element={<Facilities/>}></Route>
+        <Route exact path="/form" element={<Business/>}></Route>
       </Routes>
     </Router>
   );
