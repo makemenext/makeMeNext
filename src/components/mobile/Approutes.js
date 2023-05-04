21e6cdf69d4d4f3aafcda907ec7c66ea72918c69
Divesh Khatri <18divesh18@gmail.com>
2023-05-04 11:59:46 +0530

    temp changes

--- src/components/mobile/Approutes.js
+++ src/components/mobile/Approutes.js
@@ -1,15 +1,18 @@
 import React, {useEffect} from "react";
-// import Loginform from "./Loginform";
+import Loginform from "./Loginform";
 import Header from "./Header";
 import Showsalon from "./Showsalon";
 import Changingtext from "./Changingtext"
 import Card from "./Card";
 import Signup from "./Signup";
 import Search from "./Search";
+import Checkprice from "./Checkprice";
+import Bookingconf from "./Bookingconf";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "./demo.css";
 import Offers from "./Offers";
+import Price from "./Price";
 
 function Approutes() {
   return (
@@ -22,6 +25,9 @@ function Approutes() {
         <Route exact path="/services" element={<Showsalon />}></Route>
         <Route exact path="/search" element={<Search />}></Route>
         <Route exact path="/card" element={<Card />}></Route>
+        <Route exact path="/price" element={<Price />}></Route>
+        <Route exact path="/checkprice" element={<Checkprice />}></Route>
+        <Route exact path="/bookingconf" element={<Bookingconf />}></Route>
       </Routes>
     </Router>
   );
