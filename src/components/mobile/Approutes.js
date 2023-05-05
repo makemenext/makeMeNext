acbe2b16457ce11a813fd47df7365de39169c40d
Aashutosh Soni <soniaashu083@gmail.com>
2023-05-05 18:52:18 +0530

    Fixed

--- src/components/mobile/Approutes.js
+++ src/components/mobile/Approutes.js
@@ -6,11 +6,13 @@ import Changingtext from "./Changingtext"
 import Card from "./Card";
 import Signup from "./Signup";
 import Search from "./Search";
+import Checkprice from "./Checkprice";
+import Bookingconf from "./Bookingconf";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "./demo.css";
-import Facilities from "./Facilities";
-import Business from "./Business";
+import Offers from "./Offers";
+import Price from "./Price";
 
 
 function Approutes() {
@@ -19,6 +21,15 @@ function Approutes() {
     <Router>
       <Routes>
         <Route exact path="/" element={<div>hello</div>}></Route>
+
+        <Route exact path="/login" element={<Loginform />}></Route>
+        <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="/services" element={<Showsalon />}></Route>
+        <Route exact path="/search" element={<Search />}></Route>
+        <Route exact path="/card" element={<Card />}></Route>
+        <Route exact path="/price" element={<Price />}></Route>
+        <Route exact path="/checkprice" element={<Checkprice />}></Route>
+        <Route exact path="/bookingconf" element={<Bookingconf />}></Route>
         <Route exact path="/login" element={<Loginform/>}></Route>
         <Route exact path="/signup" element={<Signup/>}></Route>
         <Route exact path="/services" element={<Showsalon/>}></Route>
@@ -26,7 +37,7 @@ function Approutes() {
         {/* <Route exact path="/card" element={<Card/>}></Route>  */}
         <Route exact path="/offers" element={<Facilities/>}></Route>
         <Route exact path="/form" element={<Business/>}></Route>
-      
+
       </Routes>
     </Router>
   );
