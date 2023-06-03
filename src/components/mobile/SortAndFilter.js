57e66ba291b45ac140bd594b3ba2e274c8266520
Shubham Goyal <goyalshubham247@gmail.com>
2023-06-03 19:13:47 +0530

    sort and filter

--- src/components/mobile/SortAndFilter.js
+++ src/components/mobile/SortAndFilter.js
@@ -1,44 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "../../assets/styles/mobile/SortAndFilter.css";
 
 
 function SortAndFilter() {
-  const [sortOption, setSortOption] = useState(''); // State for selected sort option
-  const [filterOption, setFilterOption] = useState(''); // State for selected filter option
-
-  const handleSortChange = (event) => {
-    setSortOption(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilterOption(event.target.value);
-  };
-
-  const handleApply = () => {
-    // Perform actions based on selected sort and filter options
-    // For example, you can make an API call to fetch filtered and sorted data
-    console.log('Sort option:', sortOption);
-    console.log('Filter option:', filterOption);
-  };
 
   return (
-    <div className="sort-and-filter">
-      <div className="sort-and-filter-container">
-        <select id="sortOption" value={sortOption} onChange={handleSortChange}>
-          <option value="">-- Select Sort Option --</option>
-          <option value="price">Price</option>
-          <option value="name">Name</option>
-          <option value="date">Date</option>
-        </select>
-        <select id="filterOption" value={filterOption} onChange={handleFilterChange}>
-          <option value="">-- Select Filter Option --</option>
-          <option value="category">Category</option>
-          <option value="rating">Rating</option>
-          <option value="availability">Availability</option>
-        </select>
-        <button onClick={handleApply}>Apply</button>
+      <div class="pill-button">
+        <div class="parent-sf-container">
+          <div class="options">
+            <div class="filter">
+              <img
+                src="https://cdn.igp.com/raw/upload/assets/svg-icons/Icon-Filter.svg"
+                alt="FilterImage"
+              />
+              <div className='filter-text'> FILTER </div>
+            </div>
+            <p className='seperator'></p>
+            <div class="sort">
+              <img
+                src="https://cdn.igp.com/raw/upload/assets/svg-icons/Icon-Sort.svg"
+                alt="FilterImage"
+              />
+              <div className='sort-text'> SORT </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
